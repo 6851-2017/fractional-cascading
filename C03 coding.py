c8f3036418fd95556617097bd09d3351bc05977d
Jackie Bredenberg <jamb@mit.edu>
2017-09-21 10:33:37 -0400

    Oops, was editing in wrong directory

--- C03 coding.py
+++ C03 coding.py
@@ -57,9 +57,11 @@ class CascadeNode:
         self.successor = None
         self.predecessor = None
         self.parent = None  # the one in a different linked list that's the same
-        # TODO actually maintain the next two
+        # TODO actually maintain the next few
         self.next_real_item = None  # if it was in the original list, itself; if not, the next one that was
         self.prev_real_item = None  # if it was in the original list, itself; if not, the previous one that was
+        self.next_cascaded_item = None  # if it was in the cascaded list, itself; if not, the next one that was
+        self.prev_cascaded_item = None  # if it was in the cascaded list, itself; if not, the previous one that was
 
     def link(self, n2):
         self.successor = n2
@@ -85,34 +87,51 @@ class Cascader:
     def extend_lists(self):
         nl = []  # nodes from the previous extended linked list that are getting cascaded up
         for l in self.lists:
-            recent_node = None
-            l_index = 0
-            nl_index = 0
+            empty_node = CascadeNode(None)
+            empty_node.next_real_item = empty_node
+            empty_node.prev_real_item = empty_node
+            empty_node.next_cascaded_item = empty_node
+            empty_node.prev_cascaded_item = empty_node
+            if nl:
+                empty_node.parent = nl[0]  # empty node cascaded up from last list
+            new_extended_list = empty_node  # the next extended linked list points to the first node in itself
             
-            if not nl or l[0] < nl[0].value:
-                l_val = l[l_index]
-                recent_node = CascadeNode(l_val)
-                l_index += 1
-            else:
-                nl_val = nl[nl_index].value
-                recent_node = CascadeNode(nl_val)
-                recent_node.parent = nl[nl_index]
-                nl_index += 1
-            new_extended_list = recent_node  # the next extended linked list points to the first node in itself
+            recent_node = empty_node
+            recent_real_node = empty_node
+            recent_cascaded_node = empty_node
+            l_index = 0
+            nl_index = 1  # avoid the cascaded empty node
+            
             # merge stuff together and construct nodes
             while (l_index < len(l) and nl_index < len(nl)): # merge stuff
                 if l[l_index] < nl[nl_index].value:
                     l_val = l[l_index]
                     next_node = CascadeNode(l_val)
                     recent_node.link(next_node)
+                    p = recent_real_node.successor
+                    while (p != next_node):
+                        p.next_real_item = next_node
+                        p = p.successor
+                    next_node.next_real_item = next_node
+                    next_node.prev_real_item = next_node
+                    next_node.prev_cascaded_item = recent_cascaded_node
                     recent_node = next_node
+                    recent_real_node = next_node
                     l_index += 1
                 else:
                     nl_val = nl[nl_index].value
                     next_node = CascadeNode(nl_val)
                     next_node.parent = nl[nl_index]
                     recent_node.link(next_node)
+                    p = recent_cascaded_node.successor
+                    while (p != next_node):
+                        p.next_cascaded_item = next_node
+                        p = p.successor
+                    next_node.prev_real_item = recent_real_node
+                    next_node.prev_cascaded_item = next_node
+                    next_node.next_cascaded_item = next_node
                     recent_node = next_node
+                    recent_cascaded_node = next_node
                     nl_index += 1
                 
             # add in the stuff that didn't get merged
@@ -120,13 +139,29 @@ class Cascader:
                 for elem in l[l_index:]:
                     next_node = CascadeNode(elem)
                     recent_node.link(next_node)
+                    p = recent_real_node.successor
+                    while (p != next_node):
+                        p.next_real_item = next_node
+                        p = p.successor
+                    next_node.next_real_item = next_node
+                    next_node.prev_real_item = next_node
+                    next_node.prev_cascaded_item = recent_cascaded_node
                     recent_node = next_node
+                    recent_real_node = next_node
             else:
                 for elem in nl[nl_index:]:
                     next_node = CascadeNode(elem.value)
                     recent_node.link(next_node)
                     next_node.parent = elem
+                    p = recent_cascaded_node.successor
+                    while (p != next_node):
+                        p.next_cascaded_item = next_node
+                        p = p.successor
+                    next_node.prev_real_item = recent_real_node
+                    next_node.prev_cascaded_item = next_node
+                    next_node.next_cascaded_item = next_node
                     recent_node = next_node
+                    recent_cascaded_node = next_node
             
             self.extended_lists.append(new_extended_list)  # we finished determining this level
             nl = []  # find things we'll cascade to the next level
@@ -141,12 +176,24 @@ class Cascader:
     def find(self, x):
         # get x's predecessor/successor in every list, and return a list of those pairs
         extended_pred_succ = self.find_in_extended_lists(x)
-        return [(x.prev_real_item, y.next_real_item) for (x,y) in extended_pred_succ]        
+        return [(x.prev_real_item.value, y.next_real_item.value) for (x,y) in extended_pred_succ]        
 
     def find_in_extended_lists(self, x):
         # get x's predecessor/successor in every extended list, and return a list of those pairs
         (pred, succ) = self.find_in_last_list(x)
-        # TODO
+        pred_succ_list = [(pred, succ)]
+        for l in self.extended_lists[:-1:-1]:  # iterate backward over lists except the last
+            pred_cascade = pred.prev_cascaded_item  # TODO handle edge cases where there is no larger item in list that's cascaded, etc.
+            pred_ptr = pred_cascade.parent
+            # we only need to search up to a constant alpha forward from pred and back from succ to find the one in this list
+            while (pred.successor.value <= x):
+                pred = pred.succesor
+            if (pred.value == x):
+                succ = pred
+            else:
+                succ = pred.successor
+            pred_succ_list.append((pred, succ))
+        return pred_succ_list
 
     def find_in_last_list(self, x):
         # find x in the last list (the longest one), extended_lists[-1]; return (pred, succ)
